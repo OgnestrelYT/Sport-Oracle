53cb4e0cf459e752db33cb85240c6f4c4245712f
OgnestrelYT <arssamohvalov@gmail.com>
2024-11-23 02:24:30 +0300

    new file function

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from PyQt5 import QtWidgets, uic, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 
 
-class SaveAs(QDialog):
+class NewFile(QDialog):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Save As...")
+        self.setWindowTitle("New File...")
         
-        self.main = uic.loadUi('saveAs.ui', self)
+        self.main = uic.loadUi('new_file.ui', self)
         self.Buttons.accepted.connect(self.accept)
         self.Buttons.rejected.connect(self.reject)
     
@@ -18,7 +18,8 @@ class SaveAs(QDialog):
         if self.text == "":
             self.Error_Text.setText("Нельзя использовать пустое название")
         else:
-            MyWidget.new_text = self.text
+            s.new_filename = self.text
+            s.newFile()
             self.Error_Text.setText("")
         self.hide()
 
@@ -28,16 +29,18 @@ class SaveAs(QDialog):
 
 class MyWidget(QMainWindow):
     def __init__(self):
+        global s
         super().__init__()
         self.setWindowTitle("Sport Oracle")
         
         self.main = uic.loadUi('test.ui', self)
         self.Sessions_Table_Button.clicked.connect(self.sessions)
-        self.Save_As_Table_Button.clicked.connect(self.saveAsTable)
+        self.Create_New_File_Button.clicked.connect(self.createNewFile)
         self.Save_Table_Button.clicked.connect(self.saveTable)
         self.Load_Table_Button.clicked.connect(self.loadTable)
         self.New_Row.clicked.connect(self.newRow)
         self.Remove_Row.clicked.connect(self.removeRow)
+        s = self
     
     def newRow(self):
         self.Main_Table.insertRow(self.Main_Table.currentRow() + 1)
@@ -56,7 +59,6 @@ class MyWidget(QMainWindow):
                 self.Team_Name_Text.setText("Таблица: " + self.file[0])
                 self.Error_Text.setText("")
                 self.Save_Table_Button.setEnabled(True) # теперь можно сохранять таблицу
-                self.Save_As_Table_Button.setEnabled(True) # теперь можно сохранять таблицу как
             else:
                 self.Error_Text.setText("Не правильное разрешение файла")
         else:
@@ -65,10 +67,17 @@ class MyWidget(QMainWindow):
     def saveTable(self):
         pass
 
-    def saveAsTable(self):
+    def createNewFile(self):
         self.new_filename = ""
-        file = SaveAs()
+        file = NewFile()
         file.show()
+    
+    def newFile(self):
+        self.Team_Name_Text.setText("Таблица: " + self.new_filename)
+        self.Sessions_Table_Button.setEnabled(True)
+        a = self.Main_Table.rowCount()
+        for i in range(a):
+            self.Main_Table.removeRow(0)
     
     def loadTable(self):
         self.file = QtWidgets.QFileDialog.getOpenFileName(self, 'Выбрать файл')[0].split("/")[-1]
@@ -81,7 +90,6 @@ class MyWidget(QMainWindow):
                 self.Team_Name_Text.setText("Таблица: " + self.file)
                 self.Error_Text.setText("")
                 self.Save_Table_Button.setEnabled(True) # теперь можно сохранять таблицу
-                self.Save_As_Table_Button.setEnabled(True) # теперь можно сохранять таблицу как
                 self.Sessions_Table_Button.setEnabled(True)
                 
                 con = sqlite3.connect(self.file)
--- new_file_ui.py
+++ new_file_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\arssa\Projects\Sport\Sport-Oracle\saveAs.ui'
+# Form implementation generated from reading ui file 'c:\Users\arssa\Projects\Sport\Sport-Oracle\new_file.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
